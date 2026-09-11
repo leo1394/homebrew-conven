02ee768d9d16c0de0914310126f1c0a89d869287
leo1394 <5905686+leo1394@users.noreply.github.com>
2026-09-11 07:29:54 +0000

    conven: add 1.0.4 bottle.

--- Formula/conven.rb
+++ Formula/conven.rb
@@ -6,6 +6,13 @@ class Conven < Formula
   license "MIT"
   head "https://github.com/leo1394/homebrew-conven.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/leo1394/homebrew-conven/releases/download/conven-1.0.4"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "14db82c13d6d536c396209c1a2814b12559b8529ee083c57976ab2d7f062a49b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "707874b519401b3e6ef0bb36ee4ab24cbe21ee45922a777034711a6c40b36562"
+    sha256 cellar: :any,                 x86_64_linux:  "041604c8fa54b122c36ccc57760b538cda7bad3177c18dd3d02873b590bf5d7c"
+  end
+
   depends_on "go" => :build
 
   def install
