3f0eb4e8ee26ec8cbcf57a9d39e4e773c8e69941
yuyuelin <yuelin.yu@clobotics.com>
2026-09-21 17:33:54 +0800

    conven 1.1.0

--- Formula/conven.rb
+++ Formula/conven.rb
@@ -1,17 +1,10 @@
 class Conven < Formula
   desc "Run a focused set of local microservices with remote dependencies"
   homepage "https://github.com/leo1394/homebrew-conven"
-  url "https://github.com/leo1394/homebrew-conven/archive/refs/tags/v1.0.6.tar.gz"
-  sha256 "3a8fa07e5ee186054ae0f0166cb13c54dd6fb8090d4ccf5f7218721d81590346"
+  url "https://github.com/leo1394/homebrew-conven/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "d1841a3ed1eb56c86b83f1dbc92dbeca732ae2dd47bcc916b7789b024111d9e2"
   license "MIT"
   head "https://github.com/leo1394/homebrew-conven.git", branch: "master"
-
-  bottle do
-    root_url "https://github.com/leo1394/homebrew-conven/releases/download/conven-1.0.6"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4c755faa6f805436764b930550bf09aaaa407896596abd3918fc85d9f272a216"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cd26ac0ff8b88ae90381a2402ef25ef35720945ab3eeac0b38adaadb6c2761cf"
-    sha256 cellar: :any,                 x86_64_linux:  "747d95ec8e0eba67cc1346d635e3eab1e1c5e9359b19811fdb4e96f4bb1d2da6"
-  end
 
   depends_on "go" => :build
 
